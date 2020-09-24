bc129b23b670528f4d69a3d43a2ff5ef2d1436a7
Huy Lưu <huylv@hopnhat.vn>
2020-09-24 11:36:07 +0700

    add bin file

---
+++ bin/dev.sh
@@ -0,0 +1,6 @@
+ 
+echo "Fix images"
+HUYDEV="_currentFrame.CGImage;"
+HUYFIX="_currentFrame.CGImage ;} else { [super displayLayer:layer];"
+sed -ie "s/${HUYDEV}/${HUYFIX}/" node_modules/react-native/Libraries/Image/RCTUIImageViewAnimated.m
+echo "Done"
