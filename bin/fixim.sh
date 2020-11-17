5372b33ee8f1c8e7f5b02c003abb9bfdbf145b34
Huy Lưu <huylv@hopnhat.vn>
2020-11-17 09:57:42 +0700

    fix bug

--- bin/fixim.sh
+++ bin/fixim.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
-if grep -q super displayLayer:layer "node_modules/react-native/Libraries/Image/RCTUIImageViewAnimated.m"; then
-    echo "Done"
-else
-    echo "Fix images"
-    HUYDEV="_currentFrame.CGImage;"
-    HUYFIX="_currentFrame.CGImage ;} else { [super displayLayer:layer];"
-    sed -ie "s/${HUYDEV}/${HUYFIX}/" node_modules/react-native/Libraries/Image/RCTUIImageViewAnimated.m
-    echo "Done"
-fi
+echo "Fix images"
+HUYDEV="_currentFrame.CGImage;"
+HUYFIX="_currentFrame.CGImage ;} else { [super displayLayer:layer];"
+sed -ie "s/${HUYDEV}/${HUYFIX}/" node_modules/react-native/Libraries/Image/RCTUIImageViewAnimated.m
+echo "Done"
